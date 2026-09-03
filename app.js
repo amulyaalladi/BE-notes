21ce0f5c7892f2b93f7ce51621e6c4e41b9d8b79
amulyaalladi <amulyaalladi@gmail.com>
2026-09-03 22:31:46 +0530

    app update

--- app.js
+++ app.js
@@ -13,18 +13,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin:'http://localhost:5173',
-    methods:['GET','POST','PUT','DELETE'],
-    allowHeaders:['content-Type','Authorization'],
-    credentials:true
-}));
-
-app.use(express.json());
-
 const allowedOrigins = [
   'http://localhost:5173',
   'https://project1notesapp.netlify.app'
 ];
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+app.use(express.json());
 // use the middleware
 app.use(logger);
 
