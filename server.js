a16b440428e6d8e521f5ac114ebd374f0f19d047
amulyaalladi <amulyaalladi@gmail.com>
2026-09-04 12:20:36 +0530

    updated server

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const app = require("./app");
-const { MONGODB_URI, PORT, HOST } = require("./utils/config");
+const { MONGODB_URI, PORT } = require("./utils/config");
 const mongoose = require("mongoose");
-//const {PORT,HOST}=require('./utils/config')
 
+// On Render (and most cloud platforms) the server must bind to 0.0.0.0
+// so it's reachable from outside the container. localhost/127.0.0.1 only
+// listens on the loopback interface and causes health checks to fail.
+const HOST = "0.0.0.0";
 
-mongoose.connect(MONGODB_URI)
-.then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT,HOST, (error) => {
-    if (error) {
-        console.log('Error starting the server:', error.message);
-        return; // exits the function immediately if there is an error
-    }
-
-    console.log(`Server is running at http://${HOST}:${PORT}`);
-});
-
-})
-
-
-
-.catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message);
-});
-
-
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, HOST, (error) => {
+      if (error) {
+        console.log("Error starting the server:", error.message);
+        return;
+      }
+      console.log(`Server is running at http://${HOST}:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
